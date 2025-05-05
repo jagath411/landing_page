3e483f4aac03bbffcc372e18e274649edc5a7481
Jagath K R <108512623+jagath411@users.noreply.github.com>
2025-05-05 09:45:36 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#065f78",
+        secondary: "#FBBF24",
+        accent: "#F472B6",
+        background: "#f2f5f8",
+        neutral: "#374151",
+        "base-100": "#FFFFFF",
+        info: "#3ABFF8",
+        success: "#36D399",
+        warning: "#FBBD23",
+        error: "#F87272",
+      },
+    },
+  },
+  plugins: [],
+};
